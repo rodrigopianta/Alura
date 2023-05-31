0e9eeb5324d5c08ff9ebcc827b742cb03d199be9
Rodrigo Alves Pianta <rodrigoaps2@hotmail.com>
2023-05-31 15:07:23 -0300

    exercicios

--- Orientação objetos 2/modelos.py
+++ Orientação objetos 2/modelos.py
@@ -1,45 +1,72 @@
 class Programa:
+
     def __init__(self, nome, ano):
         self._nome = nome.title()
         self.ano = ano
         self._likes = 0
 
+
     @property
     def likes(self):
         return self._likes
 
+
     def dar_likes(self):
         self._likes += 1
+
 
     @property
     def nome(self):
         return self._nome
 
+
     @nome.setter
     def nome(self, nome):
         self._nome = nome
 
-
+    
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f" Nome: {self._nome} - Ano: {self.ano} - Duração: {self.duracao} min - Likes: {self._likes}"
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        super().__init__(nome, ano)
+        super().__init__(nome, ano) 
         self.temporadas = temporadas
+
+    def __str__(self):
+        return f" Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporadas} - Likes: {vingadores._likes}"
+
+
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('todo mundo em pânico', 1999, 100)
+demolidor = Filme('demolidor', 2016, 2)
 
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist("fim de semana", filmes_e_series)
+
+print(f"Tamanho do playlist: {len(playlist_fim_de_semana)}")
+
+for programa in playlist_fim_de_semana:
+    print(programa)
